529ea189352449c5f027d7370e9c6cc3e3f0d6b1
danielxji6 <danielxji6@gmail.com>
2016-02-21 17:40:34 -0800

    new method for socket to join or create room

--- server.js
+++ server.js
@@ -62,7 +62,6 @@ app.get('/start', function(req, res) {
   db.Game.findOne({open: true}, function(err, game) {
     if(err) { return console.log('ERROR:', err);}
     if(!game) {
-      console.log('in create game');
       db.Game.create({}, function(err, newGame) {
         if(err) { return console.log('ERROR:', err);}
         // go to the new create game play
@@ -71,23 +70,41 @@ app.get('/start', function(req, res) {
       return console.log('new game on');
     } else {
       res.redirect('/games/' + game._id);
-      return console.log('join old game');
     }
   });
 });
 
 app.get('/games/:id', function(req, res) {
   var id = req.params.id;
+  var username = req.user || 'guest'; //TODO: find username req.user.username
   var room;
-  rooms.forEach(function(ele, index) {
-    if(ele.name === id) room = ele;
+  var spot;
+  db.Game.findOne({_id: id}, function(err, game) {
+    if(err) { return console.log('ERROR:', err);}
+    if(!game.player1) {
+      spot = 1;
+      game.player1 = username || 'guest_1';
+      game.save();
+    } else if(!game.player2) {
+      spot = 2;
+      game.player2 = username || 'guest_2';
+      game.save();
+    } else {
+      spot = 3;
+      game.player3 = username || 'guest_3';
+      game.open = false;
+      game.save();
+    }
+    rooms.forEach(function(ele, index) {
+      if(ele.name === id) room = ele;
+    });
+    if(!room) {
+      room = new Room(id);
+      rooms.push(room);
+    }
+    console.log("receive", spot);
+    res.render('game', {socket_id: id, spot: spot});
   });
-  if(!room) {
-    room = new Room(id);
-    rooms.push(room);
-  }
-  console.log(rooms);
-  res.render('game');
 });
 
 // app.get('/games', function(req, res) {
@@ -109,30 +126,31 @@ var rooms = [];
 // Create a new room
 function Room(name) {
   this.name = name;
+  this.time = Date.now();
 }
-
-Room.prototype.init = function() {
-  this.nsp = io.of('/' + this.name);
-  this.nsp.on('connection', function(socket) {
-    console.log('one in the room', this.name);
-  });
-};
 
 io.on('connection', function(socket) {
   console.log('one user in');
 
-  //TODO: emit to other player
-
-  socket.on('setUser', function(data) {
-
+  socket.on('newUser', function(data) {
+    var room;
+    rooms.forEach(function(ele) { if(ele.name == data.id) room = ele.name; });
+    socket.room = room;
+    socket.join(room);
+    console.log('in the room');
+    socket.emit('setUser', {user: "", spot: data.num});
   });
+
+  // socket.on('setUser', function(data) {
+  //   console.log('setuser');
+  // });
 
   socket.on('gameFlow', function(data) {
 
   });
 
   socket.on('drawClick', function(data) {
-    socket.broadcast.emit('draw', data);
+    socket.broadcast.to(socket.room).emit('draw', data);
   });
 
   socket.on('disconnect', function() {
