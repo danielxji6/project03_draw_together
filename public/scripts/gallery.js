ec7eed58b635f2d1d8a0fec824b4acdba756fd9f
danielxji6 <danielxji6@gmail.com>
2016-02-23 17:25:05 -0800

    add gallery page

---
+++ public/scripts/gallery.js
@@ -0,0 +1,16 @@
+console.log('gallery.js load');
+
+var sourceGame = $("#game-template").html();
+var templateGame = Handlebars.compile(sourceGame);
+
+Handlebars.registerHelper('ifDraw', function(v1, v2, v3, options) {
+  if(v1) { // if(v1 && v2 && v3) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
+
+$.get('/api/games', function(games) {
+  var gameHtml = templateGame(games);
+  $('#game-list').append(gameHtml);
+});
